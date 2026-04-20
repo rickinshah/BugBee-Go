bfa405601150a9cc1729a435cab008c47313e1ed
Rickin Shah <rickinshah@proton.me>
2026-04-21 03:49:48 +0530

    remove nsfw detection

--- cmd/api/workers.go
+++ cmd/api/workers.go
@@ -10,6 +10,6 @@ func (app *application) StartWorkers(ctx context.Context) {
 	for workerID := range 1 {
 		worker.StartCommentWorker(ctx, app.models, workerID)
 		worker.StartPostVoteWorker(ctx, &app.models, workerID)
-		worker.StartNSFWWorker(ctx, app.models, workerID)
+		// worker.StartNSFWWorker(ctx, app.models, workerID)
 	}
 }
